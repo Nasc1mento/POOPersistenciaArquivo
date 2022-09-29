8af33d6842fe541adf21d645c0e47a3ee522f7d5
Nasc1mento <reisadryan16@gmail.com>
2022-09-29 19:44:04 -0300

    removendo comentarios e uns bugs

--- src/bancario/tsi/Programa.java
+++ src/bancario/tsi/Programa.java
@@ -77,10 +77,8 @@ public class Programa {
 								System.out.println("Conta não existente");
 								break;
 							}
-							
 							boolean sairMenuConta = false;
 							int opcaoMenuConta = 0;
-							
 							while (sairMenuConta == false) {
 								System.out.println("\n\n\n\nEscolha qual operação deseja realizar: \n1) Depositar;\n2) Sacar;\n3) Ver Saldo;\n4) Voltar");
 								opcaoMenuConta = sc.nextInt();
@@ -105,7 +103,6 @@ public class Programa {
 									break;
 								}
 							}
-						
 						case 5:
 							voltarMenuCliente = true;
 							break;
@@ -118,70 +115,6 @@ public class Programa {
 				sair = false;
 				break;
 			}
-			
-			
-			
-			
-			
-//			System.out.println("Escolha uma op��o:\n1) Cadastrar um cliente;\n2) Cadastrar uma conta;\n3) Sair");
-//			opcao = sc.nextInt();
-//			switch (opcao) {
-//			case 1:
-//				String nome = "";
-//				String cpf = "";
-//				System.out.println("Insira o nome do cliente: ");
-//				nome = sc.next();
-//				System.out.println("Insira o CPF: ");
-//				cpf = sc.next();
-//				Cliente cli = new Cliente(cpf, nome);
-//				pa.cadastrarCliente(cli);
-//				break;
-//			case 2:
-//				System.out.println("Insira o CPF do cliente: ");
-//				String cpfConsulta = "";
-//				int segundaOpcao = 0;
-//				boolean segundoSair = true;
-//				cpfConsulta = sc.next();
-//				Cliente cliConsulta = pa.buscarClienteCPF(cpfConsulta);
-//				if (cliConsulta != null) {
-//					while (segundoSair) {
-//						System.out.println(
-//								"\n\n\n\nEscolha as op��es para CONTA: \n1) Cadastrar uma conta;\n2) Remover uma conta;\n3) Listar contas;\n4) Sair.");
-//						segundaOpcao = sc.nextInt();
-//						switch (segundaOpcao) {
-//						case 1:
-//							String numeroConta = "";
-//							System.out.println("Insira o n�mero da conta");
-//							numeroConta = sc.next();
-//							Conta c1 = new Conta(numeroConta);
-//							cliConsulta.adicionarConta(c1);
-//							pa.atualizarCliente(cliConsulta);
-//							break;
-//						case 2:
-//
-//							break;
-//						case 3:
-//							System.out.println(cliConsulta.getContas());
-//							break;
-//						case 4:
-//							segundoSair = false;
-//							System.out.println("\n\n\n");
-//							break;
-//						default:
-//
-//							break;
-//						}
-//					}
-//				} else
-//					System.err.println("Cliente n�o encontrado!");
-//				break;
-//			case 3:
-//				sair = false;
-//				break;
-//			default:
-//				break;
-//			}
 		}
 	}
-
 }
